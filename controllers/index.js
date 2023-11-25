9da519977f5d93ec6e24a85cee0a473006bf5e43
alpha951 <20uec068@lnmiit.ac.in>
2023-11-25 17:43:46 +0530

    ADD controller setup

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  questionController: require("./question.controller.js"),
+};
---
+++ controllers/question.controller.js
@@ -0,0 +1,24 @@
+const { StatusCodes } = require("http-status-codes");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+
+async function getQuestions(req, res) {
+  try {
+    const {
+      totalMarks,
+      easyPercentage,
+      mediumPercentage,
+      difficultPercentage,
+    } = req.body;
+
+    SuccessResponse.data = questions;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    Logger.error(error);
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  getQuestions,
+};
