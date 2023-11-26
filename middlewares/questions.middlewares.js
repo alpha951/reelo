7764bd8af3b46af6ea64b496ebdb26f8d572a7ff
alpha951 <20uec068@lnmiit.ac.in>
2023-11-26 14:34:58 +0530

    UPDATE middleware corner cases

--- middlewares/questions.middlewares.js
+++ middlewares/questions.middlewares.js
@@ -23,11 +23,34 @@ function validateGetRequest(req, res, next) {
 
   if (!req.body.hardPercentage) {
     ErrorResponse.message = "Something went wrong while getting the questions";
-    ErrorResponse.explanation =
-      "hardPercentage not found in the request body";
+    ErrorResponse.explanation = "hardPercentage not found in the request body";
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
 
+  if (req.body.totalMarks < 0) {
+    ErrorResponse.message = "Something went wrong while getting the questions";
+    ErrorResponse.explanation = "totalMarks cannot be negative";
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (
+    req.body.easyPercentage +
+      req.body.mediumPercentage +
+      req.body.hardPercentage !==
+    100
+  ) {
+    ErrorResponse.message = "Something went wrong while getting the questions";
+    ErrorResponse.explanation = "Percentage should add up to 100";
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (
+    (req.body.easyPercentage < 0) |
+    (req.body.mediumPercentage < 0) |
+    (req.body.hardPercentage < 0)
+  ) {
+    ErrorResponse.message = "Something went wrong while getting the questions";
+    ErrorResponse.explanation = "Percentage cannot be negative";
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
   next();
 }
 
